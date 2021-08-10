68ec3dca1c480348fb166958c5a979c235793c9e
huijie <wszhj2008@gmail.com>
2021-08-10 16:24:29 -0700

    var loss

---
+++ segmentation_based_baselines/naive_baseline/losses/var_loss.py
@@ -0,0 +1,15 @@
+import torch
+from torch.autograd import Function
+import numpy as np
+import torch.nn.functional as F
+import torch.nn as nn
+
+class var_loss(nn.Module):
+    def __init__(self):
+        super(var_loss, self).__init__()
+
+    def forward(self, input1, input2, input3, input4):
+        input_mean = torch.mean(torch.stack([input1, input2, input3, input4]), 0)
+        input_var = ((input1-input_mean)**2 + (input2-input_mean)**2 + (input3-input_mean)**2 + (input4-input_mean)**2)/3
+        res = torch.sum(input_var)
+        return res
