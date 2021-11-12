7a53b8ebe5b0c73d7b3ace6886f5eb422394dc25
huijie <wszhj2008@gmail.com>
2021-11-12 12:14:12 -0800

    cfg

--- segmentation_based_baselines/naive_baseline/utils/train_net.py
+++ segmentation_based_baselines/naive_baseline/utils/train_net.py
@@ -58,17 +58,17 @@ def train_net(net,
         path_checkpoint = args.load_checkpoint # checkpont path
         checkpoint = torch.load(path_checkpoint) # load checkpoint
         net.load_state_dict(checkpoint['net']) # load parameter
-        # optimizer.load_state_dict(checkpoint['optimizer']) # load optimizer
+        optimizer.load_state_dict(checkpoint['optimizer']) # load optimizer
         start_epoch = checkpoint['epoch'] # set epoch
-        # lr_schedule.load_state_dict(checkpoint['lr_schedule'])
+        lr_schedule.load_state_dict(checkpoint['lr_schedule'])
         global_step = (start_epoch + 1) * (n_train // batch_size + 1)
 
-        if args.resume_epoch > 0:
-            for i in range(args.resume_epoch):
-                for j in range(args.iter_per_epoch):
-                    optimizer.zero_grad()
-                    optimizer.step()
-                lr_schedule.step()
+        # if args.resume_epoch > 0:
+        #     for i in range(args.resume_epoch):
+        #         for j in range(args.iter_per_epoch):
+        #             optimizer.zero_grad()
+        #             optimizer.step()
+        #         lr_schedule.step()
 
     else:
         global_step = 0
@@ -100,7 +100,7 @@ def train_net(net,
                 # save checkpoint
                 # if (global_step % (n_train // (batch_size)) == 0):
                 # if (epoch + 1) % 2 == 0:
-                if (global_step % ((n_train // batch_size) + 1) == 0) and (epoch + 1) % 200 == 0:
+                if (global_step % ((n_train // batch_size) + 1) == 0) and (epoch + 1) % 400 == 0:
                     # svae validation and plot
                     valid_score = eval_net(args,net,val_loader,device)
                     # scheduler.step(val_score)
